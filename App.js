157616803cbb945c1b970c0cf6acc8c0ad3db605
동현 김 <akainoo@naver.com>
2020-11-08 20:31:51 +0900

    feat #1 : start code
    - Using typescript
    - Hello World Example

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+const App: () => React$Node = () => {
+  return (
+      <Text>
+        Hello World!
+      </Text>
+  )
+};
+
+export default App;
