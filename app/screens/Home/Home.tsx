f93e9f133472f349400dae2e474604821e314e0a
동현 김 <akainoo@naver.com>
2020-12-01 20:56:25 +0900

    build #4 : CameraPermission 분리

--- app/screens/Home/Home.tsx
+++ app/screens/Home/Home.tsx
@@ -1,35 +1,9 @@
 import * as React from 'react';
-import {Text, View, Button, StyleSheet, Platform ,PermissionsAndroid} from 'react-native';
-import CameraRoll from "@react-native-community/cameraroll"
+import {Text, View, Button, StyleSheet ,PermissionsAndroid} from 'react-native';
+import {requestCameraPermission} from "../../utils/camerPermission";
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 
-const requestCameraPermission = async () => {
-  try {
-    const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
-    const hasPermission =  await PermissionsAndroid.check(permission);
-    if(hasPermission) return true;
-    const granted = await PermissionsAndroid.request(
-      permission,
-      {
-        title: "돌하르방에게 카메라를 허락해주세요!",
-        message:
-          "Cool Photo App needs access to your camera " +
-          "so you can take awesome pictures.",
-        buttonNeutral: "Ask Me Later",
-        buttonNegative: "Cancel",
-        buttonPositive: "OK"
-      }
-    );
-    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      console.log("You can use the camera");
-    } else {
-      console.log("Camera permission denied");
-    }
-  } catch (err) {
-    console.warn(err);
-  }
-};
 
 const Home = ({navigation}) => {
   const navigateToFilterScreen = () => {
---
+++ app/utils/camerPermission.ts
@@ -0,0 +1,27 @@
+import { PermissionsAndroid } from 'react-native';
+export const requestCameraPermission = async () => {
+    try {
+      const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
+      const hasPermission =  await PermissionsAndroid.check(permission);
+      if(hasPermission) return true;
+      const granted = await PermissionsAndroid.request(
+        permission,
+        {
+          title: "돌하르방에게 카메라를 허락해주세요!",
+          message:
+            "Cool Photo App needs access to your camera " +
+            "so you can take awesome pictures.",
+          buttonNeutral: "Ask Me Later",
+          buttonNegative: "Cancel",
+          buttonPositive: "OK"
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You can use the camera");
+      } else {
+        console.log("Camera permission denied");
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+};
