edc99f195d68a885fff3b614e48bede48bd29635
SaraHan774 <leisure306@gmail.com>
2020-12-07 15:52:25 +0900

    fix cameraPermission.tsx file lint error (consistent-return)

--- app/utils/cameraPermission.ts
+++ app/utils/cameraPermission.ts
@@ -1,17 +1,22 @@
 import {PermissionsAndroid} from 'react-native';
 import {permissionMessage} from '../static/permissionMessage';
 
-export const requestCameraPermission = async () => {
+export const requestCameraPermission = async (): Promise<boolean | any> => {
 	try {
 		const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
 		const hasPermission = await PermissionsAndroid.check(permission);
 
-		if (hasPermission) return true;
-		await PermissionsAndroid.request(
-			permission,
-			permissionMessage,
-		);
+		if (hasPermission) {
+			return true;
+		} else {
+			await PermissionsAndroid.request(
+				permission,
+				permissionMessage,
+			);
+			return false;
+		}
 	} catch (err) {
 		console.warn(err);
+		throw new Error("Asking camera permission failed.");
 	}
 };
