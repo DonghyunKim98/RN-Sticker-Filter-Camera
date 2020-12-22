483b037fd24eda54b8277b53f2f39907557678e3
동현 김 <akainoo@naver.com>
2020-12-22 16:41:56 +0900

    refactor : seperate static values

--- app/screens/FilterCamera/FilterCameraResultScreen.tsx
+++ app/screens/FilterCamera/FilterCameraResultScreen.tsx
@@ -1,7 +1,9 @@
-import React , {useState} from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Image, View} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 import FilterBtns from './FilterBtns';
+import {titles} from '../../static/FilterCamera/FilterBtnValue';
+import {filterStyleValue} from '../../static/FilterCamera/FilterValue';
 
 const styles = StyleSheet.create({
 	container: {
@@ -33,20 +35,11 @@ const styles = StyleSheet.create({
 	},
 });
 
-const titles = ["흑백", "필터 버튼 2번", "필터 버튼 3번", "필터 버튼 4번", "필터 버튼 5번", "필터 버튼 6번", "필터 버튼 7번"];
-
 function FilterCameraResultScreen({route}) {
-	const [filterValue, setFilterValue] = useState(styles.filter);
+	const [filterValue, setFilterValue] = useState(filterStyleValue.default);
 	const {photoUri = ''}: { photoUri: string } = route.params;
 	const FilterBtnClickListener = () : void => {
-		setFilterValue({
-			position: 'absolute',
-			width: '100%',
-			height: '100%',
-			zIndex: 2,
-			backgroundColor: '#000000',
-			opacity: 0.3,
-		});
+		setFilterValue(filterStyleValue.BlackAndWhite);
 	};
 
 	return (
