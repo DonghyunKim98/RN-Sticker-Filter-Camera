ed9d12e7e40aa6cc25c2a269aefde7b6fa85e011
SaraHan774 <leisure306@gmail.com>
2020-12-26 12:21:38 +0900

    commit before fetching another branch

--- app/screens/StickerCamera/StickerCamera.tsx
+++ app/screens/StickerCamera/StickerCamera.tsx
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
 import {Image, View, Button, StyleSheet, Text} from 'react-native';
 import ImagePicker from 'react-native-image-picker';
-import {
-	androidPermission,
-} from '../../utils/cameraPermission';
+import Draggable from 'react-native-draggable';
+
+import {androidPermission} from '../../utils/cameraPermission';
 import {stickerCameraOptions} from '../../static/imagePickerOption';
+
+const sampleSticker = require('../../assets/images/smile.png');
 
 // sample code from https://reactnative.dev/docs/permissionsandroid
 const styles = StyleSheet.create({
@@ -13,8 +15,16 @@ const styles = StyleSheet.create({
 		marginBottom: 5,
 	},
 	imageStyle: {
-		width: 100,
-		height: 200,
+		width: '100%',
+		height: '100%',
+	},
+	sampleImageStyle: {
+		width: 30,
+		height: 30,
+	},
+	stickerContainerStyle: {
+		zIndex: 0,
+		elevation: 3,
 	},
 });
 
@@ -26,7 +36,7 @@ const StickerCamera = () => {
 			console.log(response);
 			if (response.error != null) {
 				if (response.error.match('Permissions')) {
-					androidPermission("CAMERA");
+					androidPermission('CAMERA');
 				}
 			} else {
 				setPhotoUri(response.uri);
@@ -37,7 +47,7 @@ const StickerCamera = () => {
 	const onOpenGalleryClick = (): void => {
 		ImagePicker.launchImageLibrary(stickerCameraOptions, (response) => {
 			// move this to another file later
-			androidPermission("READ_EXTERNAL_STORAGE");
+			androidPermission('READ_EXTERNAL_STORAGE');
 			console.log(response);
 
 			if (response.error != null) {
@@ -66,6 +76,24 @@ const StickerCamera = () => {
 			) : (
 				<Text>이미지 없음</Text>
 			)}
+			<View style={styles.stickerContainerStyle}>
+				{/* <Draggable
+					x={75}
+					y={100}
+					renderSize={56}
+					renderColor="black"
+					renderText="A"
+					isCircle
+					shouldReverse
+					// eslint-disable-next-line no-alert
+					onShortPressRelease={() => alert('touched!!')}
+				/> */}
+				{/* <Draggable x={200} y={300} renderColor="red" renderText="B" /> */}
+				{/* <Draggable /> */}
+				<Draggable x={50} y={300}>
+					<Image style={styles.sampleImageStyle} source={sampleSticker} />
+				</Draggable>
+			</View>
 		</View>
 	);
 };
